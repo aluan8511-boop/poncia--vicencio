140efb90e50b56c0cc3c333bcaad3d6212307543
aluan8511-boop <aluan8511@gmail.com>
2025-08-14 20:27:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function showInfo(type) {
     switch(type) {
         case 'resenha':
             content = `<h2>Resenha Crítica</h2>
-                       <p>A história de Ponciá Vicêncio descreve os caminhos, as andanças, as marcas, os sonhos e os desencantos da protagonista. A autora traça a trajetória da personagem da infância à idade adulta, analisando seus afetos e desafetos e seu envolvimento com a família e os amigos. Discute a questão da identidade de Ponciá, centrada na herança identitária do avô e estabelece um diálogo entre o passado e o presente, entre a lembrança e a vivência, entre o real e o imaginado.</p>`;
+                       <p>A história de Ponciá Vicêncio descreve os caminhos, as andanças, as marcas, os sonhos e os desencantos da protagonista. A autora traça a trajetória da personagem da infância à idade adulta, analisando seus afetos e desafetos e seu envolvimento com a família e os amigos. Discute a questão da identidade de Ponciá, centrada na herança identitária do avô e estabelece um diálogo entre o passado e o presente, entre a lembrança e a vivência, entre o real e o imaginado.</p>
+                       <p>Ponciá Vicêncio ganhou uma versão em inglês, permitindo que leitores de diferentes culturas tenham acesso à obra. A tradução preserva a essência da narrativa de Conceição Evaristo, destacando questões de identidade, memória e pertencimento, que são universais e relevantes em diversos contextos sociais.</p>`;
             break;
         case 'entrevista':
             content = `<h2>Entrevista com Conceição Evaristo</h2>
@@ -18,6 +19,10 @@ function showInfo(type) {
         case 'estudo':
             content = `<h2>Estudo Acadêmico sobre Ponciá Vicêncio</h2>
                        <p>Este artigo aborda a novela de Conceição Evaristo, refletindo sobre Memória e Ficción. A narrativa está constituída por elementos interconectados pelas memórias de Ponciá, explorando a condição do negro no Brasil e temas como identidade, racismo e ancestralidade.</p>`;
+            break;
+        case 'ingles':
+            content = `<h2>Versão em Inglês</h2>
+                       <p>A obra "Ponciá Vicêncio" foi traduzida para o inglês, tornando-se acessível a um público mais amplo. A tradução mantém a riqueza da linguagem de Conceição Evaristo e aborda temas como identidade e pertencimento, que são centrais na narrativa. Essa versão oferece uma oportunidade para leitores internacionais explorarem a complexidade da experiência negra no Brasil e a luta por reconhecimento e dignidade.</p>`;
             break;
         default:
             content = '<p>Nenhuma informação disponível.</p>';
